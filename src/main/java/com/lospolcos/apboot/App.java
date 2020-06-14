6b1098ec417da3f598f248b4f4eb497b15f27a8d
Samuel Rohrer <srohrer32@gmail.com>
2020-06-14 13:34:39 -0400

    initil commit

---
+++ src/main/java/com/lospolcos/apboot/App.java
@@ -0,0 +1,20 @@
+package com.lospolcos.apboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App 
+{
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		logger.info("the application hath begun");
+	}
+	
+}
---
+++ src/test/java/com/lospolcos/apboot/AppTest.java
@@ -0,0 +1,37 @@
+package com.lospolcos.apboot;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Application.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static TestSuite suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Assertibility Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
